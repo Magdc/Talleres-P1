ed7cb181468e45a5292932d0d364e796a525b078
Magdc <117609790+Magdc@users.noreply.github.com>
2024-08-01 17:35:49 -0500

    visualizacion de películas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
-
     #return render(request, 'home.html') 
-    return render(request, 'home.html',{'name':'Miguel Alejandro Gomez Duque'})
+    #return render(request, 'home.html',{'name':'Miguel Alejandro Gomez Duque'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 
 
